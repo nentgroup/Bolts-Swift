8722db4847a305b85c6444bb95ce80e501d9938f
Herman Olsson <h@herman.io>
2020-10-14 21:27:55 +0200

    Bump iOS requirement for SPM to iOS 9

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "BoltsSwift",
     platforms: [.macOS(.v10_10),
-                .iOS(.v8),
+                .iOS(.v9),
                 .tvOS(.v9),
                 .watchOS(.v3)],
     products: [
